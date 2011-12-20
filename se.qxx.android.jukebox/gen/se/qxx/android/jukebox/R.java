c839f734196824f56e4629be36e37b67db6faedc
chris <chris@8565fa27-0427-49c4-bedb-6e22af59f9d2>
2011-12-20 00:54:28 +0000

    Added listview to main window

--- se.qxx.android.jukebox/gen/se/qxx/android/jukebox/R.java
+++ se.qxx.android.jukebox/gen/se/qxx/android/jukebox/R.java
@@ -20,6 +20,7 @@ public final class R {
         public static final int button1=0x7f060004;
         public static final int button2=0x7f060005;
         public static final int editText1=0x7f060000;
+        public static final int listView1=0x7f060006;
         public static final int radio0=0x7f060002;
         public static final int radio1=0x7f060003;
         public static final int radioGroup1=0x7f060001;
