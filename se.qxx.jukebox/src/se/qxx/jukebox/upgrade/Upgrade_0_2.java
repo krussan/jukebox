64b8b49bf580b9f8f84d5194630897e39d66518e
chris <chris@8565fa27-0427-49c4-bedb-6e22af59f9d2>
2012-09-04 22:48:42 +0000

    Added some more upgrade scripts for 0.2

--- se.qxx.jukebox/src/se/qxx/jukebox/upgrade/Upgrade_0_2.java
+++ se.qxx.jukebox/src/se/qxx/jukebox/upgrade/Upgrade_0_2.java
@@ -6,6 +6,9 @@ public class Upgrade_0_2 implements IIncrimentalUpgrade {
 
 	static final String[] DbScripts = {
 		"ALTER TABLE Movie ADD duration int"
+	  , "ALTER TABLE Movie ADD rating varchar(5)"
+	  , "ALTER TABLE Movie ADD director varchar(100)"
+	  , "ALTER TABLE Movie ADD story varchar(1024)"
 	  , "CREATE TABLE Genre (ID int IDENTITY NOT NULL PRIMARY KEY, genreName varchar(50) NOT NULL)"
 	  , "CREATE TABLE MovieGenre (_movie_ID int NOT NULL REFERENCES Movie(ID), _genre_ID int NOT NULL REFERENCES Genre(ID) )"
 	};
@@ -22,10 +25,13 @@ public class Upgrade_0_2 implements IIncrimentalUpgrade {
 
 	@Override
 	public void performUpgrade() throws UpgradeFailedException {
-		
-		// get duration		
 		Upgrader.runDatabasescripts(DbScripts);
 
+		//TODO: perform upgrade for version 0.2
+		// get duration	
+		// update genres
+		// get rating
+		// get director
+		// get story
 	}
-
 }
