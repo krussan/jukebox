b79d1433571636f4314b8ec1390cfa0f48aa6525
chris <chris@8565fa27-0427-49c4-bedb-6e22af59f9d2>
2012-09-17 12:49:07 +0000

    Added more code to NFOBuilder (not finished though)

--- se.qxx.jukebox/src/se/qxx/jukebox/builders/NFOScanner.java
+++ se.qxx.jukebox/src/se/qxx/jukebox/builders/NFOScanner.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
@@ -49,10 +50,12 @@ public class NFOScanner {
 	public List<NFOLine> scan() {
 		ArrayList<NFOLine> list = new ArrayList<NFOLine>();
 		
+		InputStreamReader r = null;
+		BufferedReader bf = null;
 		try {
 			
-			InputStreamReader r = new InputStreamReader(new FileInputStream(this.nfoFile), "ISO-8859-1");
-			BufferedReader bf = new BufferedReader(r);
+			r = new InputStreamReader(new FileInputStream(this.nfoFile), "ISO-8859-1");
+			bf = new BufferedReader(r);
 			
 			
 			StringBuilder sb  = new StringBuilder();
@@ -74,6 +77,16 @@ public class NFOScanner {
 			}
 		} catch (Exception e) {
 			Log.Error(String.format("NFOScanner error while parsing %s:: ", this.nfoFile.getName()), LogType.FIND, e);
+		}
+		finally {
+			try {
+				if (bf!=null)
+					bf.close();
+				
+				if (r!=null)
+					r.close();
+			} catch (IOException e) {
+			}
 		}
 		
 		return list;
---
+++ se.qxx.jukebox/src/se/qxx/jukebox/builders/NfoBuilder.java
@@ -0,0 +1,33 @@
+package se.qxx.jukebox.builders;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
+import java.util.regex.Pattern;
+
+import se.qxx.jukebox.Util;
+import se.qxx.jukebox.domain.JukeboxDomain.Movie;
+
+public class NfoBuilder extends MovieBuilder {
+	
+	
+	@Override
+	public Movie extractMovie(String filepath, String filename) {
+		Movie m = null;
+		try {
+			String filenameWithoutExt = Util.getFilenameWithoutExtension(filename);
+			File f = new File(String.format("%s/%s.nfo", filepath, filename));
+			if (f.exists()) {
+				NFOScanner scanner = new NFOScanner(f);
+				List<NFOLine> lines = scanner.scan();
+
+			}
+		} catch (Exception e) {
+		}
+		
+		return m; 
+	}
+}
