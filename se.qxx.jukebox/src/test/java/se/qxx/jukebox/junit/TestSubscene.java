079118e96e895e4056bc7ec442308f302179b333
krussan <christian@qxx.se>
2016-11-14 20:08:48 +0100

    wip: adding test for Subscene

--- se.qxx.jukebox/src/test/java/se/qxx/jukebox/junit/TestSubscene.java
+++ se.qxx.jukebox/src/test/java/se/qxx/jukebox/junit/TestSubscene.java
@@ -3,6 +3,10 @@ package se.qxx.jukebox.junit;
 import static org.junit.Assert.*;
 
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.xml.bind.JAXBException;
 
@@ -10,14 +14,18 @@ import org.apache.commons.lang3.StringUtils;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
+import se.qxx.jukebox.WebResult;
 import se.qxx.jukebox.WebRetriever;
+import se.qxx.jukebox.domain.JukeboxDomain.Movie;
 import se.qxx.jukebox.settings.Settings;
 import se.qxx.jukebox.settings.JukeboxListenerSettings.SubFinders.SubFinder;
 import se.qxx.jukebox.settings.JukeboxListenerSettings.SubFinders.SubFinder.SubFinderSettings;
+import se.qxx.jukebox.subtitles.SubFile;
 import se.qxx.jukebox.subtitles.Subscene;
 
 @RunWith(PowerMockRunner.class)
@@ -25,6 +33,10 @@ import se.qxx.jukebox.subtitles.Subscene;
 public class TestSubscene {
 
 	private SubFinderSettings subFinderSettings = null;
+	private final String searchResult = "";
+	private final String listResult = "";
+	private final String download1Result = "";
+	private final String download2Result = "";
 	
 	@Before
 	public void init() throws IOException, JAXBException {
@@ -41,13 +53,28 @@ public class TestSubscene {
 	}
 	
 	@Test
-	public void test() {
+	public void test() throws IOException {
 		assertNotNull(subFinderSettings);
 		
 		PowerMockito.mockStatic(WebRetriever.class);
 
 		Subscene ss = new Subscene(subFinderSettings);
 		
+		WebResult result = new WebResult(new URL("http://localhost/subtitiles/english/mockito"), "", false);
+		Mockito.when(WebRetriever.getWebResult("https://subscene.com/subtitles/title?q=Mockito")).thenReturn(result);
+	
+		Movie m = Movie.newBuilder()
+				.setID(1)
+				.setIdentifiedTitle("Mockito")
+				.setTitle("Mockito")
+				.build();
+		
+		List<String> lang = new ArrayList<String>();
+		lang.add("English");
+		
+		List<SubFile> files = ss.findSubtitles(m, lang);
+		
+		assertEquals(2, files.size());
 	}
 
 }
