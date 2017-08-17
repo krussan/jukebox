7cf78dbaca7606e23163badc9886b1997f4590e9
vagrant <vagrant@localhost.localdomain>
2017-08-17 20:53:12 +0000

    Fixed error in sql

--- se.qxx.jukebox/sql/getTop5SubtitleQueueEpisode.sql
+++ se.qxx.jukebox/sql/getTop5SubtitleQueueEpisode.sql
@@ -2,7 +2,7 @@ SELECT S.title, SS.seasonNumber, E.episodeNumber, SQ.subtitleRetreiveResult
 FROM Series S
 INNER JOIN SeriesSeason_Season SSS ON SSS._series_ID = S.ID
 INNER JOIN Season SS ON SSS._season_ID = SS.ID
-INNER JOIN SeasonEpisode_Episode SEE ON SEE._season_ID = S.ID
+INNER JOIN SeasonEpisode_Episode SEE ON SEE._season_ID = SS.ID
 INNER JOIN Episode E ON SEE._episode_ID = E.ID
 INNER JOIN SubtitleQueue SQ ON E._subtitleQueue_ID = SQ.ID
 WHERE SQ.subtitleRetreiveResult = 0
