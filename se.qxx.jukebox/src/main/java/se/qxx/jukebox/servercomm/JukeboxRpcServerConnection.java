e41706d9baf7cf919f10d39aad75535d43f78464
Christian Thunström <christian@qxx.se>
2017-09-04 19:53:17 +0000

    wip: partial list

--- se.qxx.jukebox/src/main/java/se/qxx/jukebox/servercomm/JukeboxRpcServerConnection.java
+++ se.qxx.jukebox/src/main/java/se/qxx/jukebox/servercomm/JukeboxRpcServerConnection.java
@@ -57,18 +57,35 @@ public class JukeboxRpcServerConnection extends JukeboxService {
 
 		Log.Debug("ListMovies", LogType.COMM);
 		
+		
 		String searchString = request.getSearchString();
-		List<Movie> list = DB.searchMoviesByTitle(searchString, true, true);
-		List<Series> listSeries = DB.searchSeriesByTitle(searchString, true, true);
+		
+		List<Movie> movies = new ArrayList<Movie>();
+		List<Series> series = new ArrayList<Series>(); 
+		
+		switch (request.getRequestType()) {
+		case TypeMovie:
+			movies = DB.searchMoviesByTitle(searchString, true, true);
+			break;
+		case TypeSeries:
+			series = DB.searchSeriesByTitle(searchString, true, true);
+			break;
+		case TypeSeason:
+			series.add(DB.getSeries(request.getSeriesID()));
+			break;
+		case TypeEpisode:
+			//TODO!
+			break;
+		}
 		
 		if (!request.getReturnFullSizePictures()) {
-			list = removeFullSizePicturesAndSubsFromMovies(list);
-			listSeries = removeFullSizePicturesAndSubsFromSeries(listSeries);
+			movies = removeFullSizePicturesAndSubsFromMovies(list);
+			series = removeFullSizePicturesAndSubsFromSeries(listSeries);
 		}
 		
 		JukeboxResponseListMovies lm = JukeboxResponseListMovies.newBuilder()
-				.addAllMovies(list)
-				.addAllSeries(listSeries)
+				.addAllMovies(movies)
+				.addAllSeries(series)
 				.build();
 		done.run(lm);
 	}
