5c8b87a74a0708cb692f9765bd74c05f926dbc8f
Christian Thunström <christian@qxx.se>
2018-01-27 21:48:49 +0000

    protecting identity

--- ci/decrypt_files.sh
+++ ci/decrypt_files.sh
@@ -2,3 +2,4 @@
 
 openssl aes-256-cbc -d -k "$FILE_PASSWORD" -in ci/keystore.enc -out ci/keystore
 openssl aes-256-cbc -d -k "$TRAVIS_IDENTITY" -in ci/travis.enc -out ~/.ssh/id_rsa
+chmod 600 ~/.ssh/id_rsa
