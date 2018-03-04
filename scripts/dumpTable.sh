fcaf676f2b4cc443abeaae69b675f0fcc065c4e9
Christian Thunström <christian@qxx.se>
2018-03-04 22:08:19 +0000

    Removed BlobData from perl replace

--- scripts/dumpTable.sh
+++ scripts/dumpTable.sh
@@ -7,8 +7,14 @@ echo "-- $TABLE:";
 COLS=`"$SQLITE" "$DB" "pragma table_info($TABLE)" | cut -d'|' -f2 `
 COLS_CS=`echo $COLS | sed 's/ /,/g'`
 
-echo -e ".mode insert\nselect $COLS_CS from $TABLE;\n" | \
-    "$SQLITE" "$DB" | \
-    sed "s/^INSERT INTO table/INSERT INTO $TABLE ($COLS_CS)/" | \
-    perl -ne 's/\n//g; print;' | \
-    perl -ne 's/INSERT\s*INTO/\nINSERT INTO/g;print;'
+if [ "$TABLE" != "BlobData" ]; then
+    echo -e ".mode insert\nselect $COLS_CS from $TABLE;\n" | \
+       "$SQLITE" "$DB" | \
+       sed "s/^INSERT INTO table/INSERT INTO $TABLE ($COLS_CS)/" | \
+       perl -ne 's/\n//g; print;' | \
+       perl -ne 's/INSERT\s*INTO/\nINSERT INTO/g;print;'
+else
+    echo -e ".mode insert\nselect $COLS_CS from $TABLE;\n" | \
+       "$SQLITE" "$DB" | \
+       sed "s/^INSERT INTO table/INSERT INTO $TABLE ($COLS_CS)/"
+fi
