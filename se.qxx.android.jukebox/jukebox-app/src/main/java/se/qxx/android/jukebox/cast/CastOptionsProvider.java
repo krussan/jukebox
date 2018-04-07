4f87040930ac895d6aa634fd20ca08e17cbb309f
Christian Thunström <christian@qxx.se>
2018-04-07 19:41:00 +0000

    broken. initial commit for caf

---
+++ se.qxx.android.jukebox/jukebox-app/src/main/java/se/qxx/android/jukebox/cast/CastOptionsProvider.java
@@ -0,0 +1,8 @@
+package se.qxx.android.jukebox.cast;
+
+/**
+ * Created by vagrant on 4/7/18.
+ */
+
+public class CastOptionsProvider implements OptionsProvider {
+}
--- se.qxx.android.jukebox/jukebox-app/src/main/java/se/qxx/android/jukebox/cast/ChromeCastConfiguration.java
+++ se.qxx.android.jukebox/jukebox-app/src/main/java/se/qxx/android/jukebox/cast/ChromeCastConfiguration.java
@@ -1,12 +1,17 @@
 package se.qxx.android.jukebox.cast;
 
 import android.app.Activity;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.graphics.Color;
 import android.view.Menu;
 import android.view.MenuInflater;
 
 import com.google.android.gms.cast.CastMediaControlIntent;
 import com.google.android.gms.cast.TextTrackStyle;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.common.GooglePlayServicesUtil;
 import com.google.android.libraries.cast.companionlibrary.cast.BaseCastManager;
 import com.google.android.libraries.cast.companionlibrary.cast.CastConfiguration;
 import com.google.android.libraries.cast.companionlibrary.cast.VideoCastManager;
@@ -37,12 +42,39 @@ public class ChromeCastConfiguration {
 
     public static void initialize(Activity activity) {
         if (isChromeCastActive()) {
-            BaseCastManager.checkGooglePlayServices(activity);
+            ChromeCastConfiguration.checkGooglePlayServices(activity);
 
             CastConfiguration options = ChromeCastConfiguration.get();
 
             VideoCastManager.initialize(activity, options);
         }
+    }
+
+    /**
+     * A utility method to validate that the appropriate version of the Google Play Services is
+     * available on the device. If not, it will open a dialog to address the issue. The dialog
+     * displays a localized message about the error and upon user confirmation (by tapping on
+     * dialog) will direct them to the Play Store if Google Play services is out of date or
+     * missing, or to system settings if Google Play services is disabled on the device.
+     */
+    public static boolean checkGooglePlayServices(final Activity activity) {
+        final int googlePlayServicesCheck = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(activity);
+
+        switch (googlePlayServicesCheck) {
+            case ConnectionResult.SUCCESS:
+                return true;
+            default:
+                Dialog dialog = GoogleApiAvailability.getInstance().getErrorDialog(activity, googlePlayServicesCheck, 0);
+
+                dialog.setOnCancelListener(new DialogInterface.OnCancelListener() {
+                    @Override
+                    public void onCancel(DialogInterface dialogInterface) {
+                        activity.finish();
+                    }
+                });
+                dialog.show();
+        }
+        return false;
     }
 
     public static boolean isChromeCastActive() {
