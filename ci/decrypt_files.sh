c8a8f268d2441debd1afb473f62b2e2bd21183a0
Christian Thunström <christian.thunstrom@eqtpartners.com>
2024-07-08 13:56:19 +0200

    update keystore decrypt

--- ci/decrypt_files.sh
+++ ci/decrypt_files.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-openssl aes-256-cbc -d -k "$FILE_PASSWORD" -in ci/keystore.enc -out ci/keystore
+openssl aes-256-cbc -d -k "$FILE_PASSWORD" -in ci/keystore2.enc -out ci/keystore2
 openssl aes-256-cbc -d -k "$FILE_PASSWORD" -in ci/api2.enc -out ci/api.json
 
 chmod 600 ~/.ssh/id_rsa
