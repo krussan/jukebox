d1ec6d5ccc6d64300dceea96eb4c22138aea320c
Christian Thunström <christian.thunstrom@eqtpartners.com>
2024-07-08 14:04:16 +0200

    fixesw

--- ci/decrypt_files.sh
+++ ci/decrypt_files.sh
@@ -2,5 +2,3 @@
 
 openssl aes-256-cbc -d -k "$FILE_PASSWORD" -in ci/keystore2.enc -out ci/keystore2
 openssl aes-256-cbc -d -k "$FILE_PASSWORD" -in ci/api2.enc -out ci/api.json
-
-chmod 600 ~/.ssh/id_rsa
